478f45040114395872638c26dd6053eb2786336f
Valenchongo <107526652+Valenchongo@users.noreply.github.com>
2022-07-28 01:09:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ contador = document.querySelector(".contador");
 bocaa = document.querySelector(".bocaa");
 position_dinohorizontal = parseInt(window.getComputedStyle(dino).getPropertyValue("left"));
 sombrero = document.querySelector(".sombreropre10");
+tubo =  document.querySelector(".tubo");
+bujero =  document.querySelector(".agujero");
+suelo = document.querySelector(".suelo")
 sombreropuesto = false;
 i = 0;
 j=0;
@@ -12,6 +15,56 @@ v=2;
 posicion_dinovertical =0;
 puntuacion = 0;
 saltando = false
+
+function saltar (){  //funcion salatar
+  
+    j=0;
+    l = 0;
+    if(sombreropuesto == false){
+   if (posicion_dinovertical >= 371){
+    
+  jump = setInterval(() => {
+    h = posicion_dinovertical; 
+    h = h-12;
+    j++;
+    dino.style.top = h + "px";
+    if(j >= 13){
+        clearInterval(jump);
+        i=1;
+        j=0;
+       } 
+       saltando = true;     
+  }, 10);  
+}
+}else{
+  jump = setInterval(() => {
+    console.log("ejecutando")
+    h = posicion_dinovertical; 
+    h = h-8;
+    j++;
+    dino.style.top = h + "px";
+    if(j >= 25){
+        clearInterval(jump);
+        i=1;
+        j=0;
+       } 
+       saltando = true;     
+  }, 4);  
+}
+}  
+
+bujero.addEventListener("animationiteration" ,()=>{
+  posicionagujero = -((Math.random()*350)+314)
+  posicionmaxbujero =570+posicionagujero;
+  posicionminbujero = 670+posicionagujero;
+  bujero.style.top = posicionagujero +"px";
+  posagujero = bujero.style.top
+  console.log(posagujero)
+  if(puntuacion>=11){
+    puntuacion++
+  contador.innerHTML = puntuacion;
+  }
+})
 
 cactus.addEventListener("animationiteration",()=>{
 puntuacion ++;
@@ -21,6 +74,7 @@ if((puntuacion % 10) == 0 && puntuacion != 0){
   contador.style.color = "blueviolet"
 }else{
   contador.style.color = "white"
+  
 }
 contador.innerHTML = puntuacion;
 })
@@ -31,12 +85,20 @@ setInterval(() => {   //intevalo en el que evalua constantemente los cambios rea
     posicion_dinovertical = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     posicion_sombrerovertical = parseInt(window.getComputedStyle(sombrero).getPropertyValue("top"));
 
+
+    if(sombreropuesto == false){     /*esto sucede en el game cuando no tenemos el sombrero*/ 
+    
+    tubo.style.backgroundColor ="none"
+    bujero.style.backgroundColor = " none"
+    suelo.style.display = "flex"
+    cactus.style.display = "flex"
+    
     if( 80>= position_cactushorizontal && posicion_dinovertical  >= position_cactusvertical){
-        alert("perdiste, tu puntuacion fue de " +puntuacion);
+       
+      alert("perdiste, tu puntuacion fue de " +puntuacion);
         
-        if(sombreropuesto == true){
-          sombreropuesto = false;
-          sombrero.classList.replace("sombreropuesto","sombreropre10");
+        if(puntuacion == 10){
+          sombrero.classList.replace("sombrero","sombreropre10");
         }
         puntuacion = -1;
         v = 2;
@@ -52,51 +114,55 @@ if(puntuacion == 11){
   sombreropuesto = true;
 
 }
-
-if(sombreropuesto == true){
-  console.log("culo")
-   sombrero.style.top = posicion_dinovertical-170 +"px";
+if((posicion_dinovertical >= 0) && (posicion_dinovertical<=370)){
+  h = posicion_dinovertical; 
+      h = h+3; 
+      dino.style.top = h + "px";
+      bocaa.classList.replace("boca","bocaabierta")
+      
+}else{
+bocaa.classList.replace("bocaabierta","boca")
 }
 
+}
+if(sombreropuesto == true){   /*esto sucede en el game cuando tenemos el sombrero puesto*/ 
+  tubo.style.backgroundColor ="green"
+  tubo.style.display ="flex"
+  bujero.style.display = " flex"
+  bujero.style.backgroundColor= "#040015"
+  suelo.style.display = "none"
+  cactus.style.display = "none"
+  leftpj = parseInt(window.getComputedStyle(dino).getPropertyValue("left"));
+  lefttubo =parseInt(window.getComputedStyle(tubo).getPropertyValue("left"));
+  posicionpj = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+  sombrero.style.top = posicion_dinovertical-70 +"px";
+  
+  
 
-if((posicion_dinovertical >= 0) && (posicion_dinovertical<=370)){
     h = posicion_dinovertical; 
         h = h+3; 
         dino.style.top = h + "px";
         bocaa.classList.replace("boca","bocaabierta")
         
-}else{
-  bocaa.classList.replace("bocaabierta","boca")
+  
+
+  if(lefttubo<=leftpj+25){
+    if( (posicionpj <= posicionmaxbujero+15 )|| (posicionpj>=posicionminbujero+20) ){
+      alert("tu puntuacion es de "+puntuacion)
+      puntuacion = 0;
+      contador.innerHTML = puntuacion;
+      sombreropuesto = false;     
+      tubo.style.display = "none"
+          bujero.style.display ="none"
+          sombreropuesto = false;
+          sombrero.classList.replace("sombreropuesto","sombreropre10");
+          dino.style.top = 370 + "px";
+    }        
+  }   
 }
-    
-    
    
 }, 10);
 
-function saltar (){  //funcion salatar
-    j=0;
-    l = 0;
-   if (posicion_dinovertical >= 371){
-    
-  jump = setInterval(() => {
-    h = posicion_dinovertical; 
-    h = h-4.3;
-    j++;
-    dino.style.top = h + "px";
-    if(j >= 25){
-        clearInterval(jump);
-        i=1;
-        j=0;
-       }
-       
-       saltando = true;
-       
-  }, 10);
-  
-}
+juego.addEventListener("click",saltar)
 
-  i = 1;
-}
-
-juego.addEventListener("click",saltar);
 
